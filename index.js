322e70ab5a9ef4c08c991f5e13f75b8f857bd5f9
MattW-R <Matthew.wr89@gmail.com>
2021-04-09 15:52:33 +0100

    Made articles expandable by focussing using JS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const addExpandEffectToBoxes = () => {
     document.querySelectorAll('.box').forEach(box => {
-        box.addEventListener('click', e => {
+        box.addEventListener('mousedown', e => {
             e.preventDefault()
             if (box.classList.contains('expanded')) {
                 box.classList.remove('expanded')
+                if (box.contains(document.activeElement)) {
+                    document.activeElement.blur()
+                }
             } else {
                 box.classList.add('expanded')
+            }
+        })
+    })
+
+    document.querySelectorAll('.box p, .box h2, .box img').forEach(boxChild => {
+        boxChild.addEventListener('focus', e => {
+            e.preventDefault()
+            if (!boxChild.parentElement.classList.contains('expanded')) {
+                boxChild.parentElement.classList.add('expanded')
             }
         })
     })
